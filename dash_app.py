ef2bb7ac85444691727ddf2f9528467c95e25b54
chiquitosaf <chiquitosaf@gmail.com>
2024-08-10 10:53:04 +0700

    add header in dash_app.py and closing ipynb section except EDA section

--- dash_app.py
+++ dash_app.py
@@ -1,11 +1,20 @@
-from dash_app import Dash, html
+from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(
+ external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 
 
-app.layout = html.Div([
-    html.H1("Test")
-])
+app.layout = html.Div(children=[
+    #header
+    html.Div(children=[
+        html.H1(children="Analisis Data Harga Bahan Pokok Bulanan Tahun 2019 - 2023 Kab. Banyumas",
+                id="main-title"),
+    html.Hr(id="title-line")        
+    ], id="main-header")
+
+], id="main-container", style={"margin" : "50px"})
 
 if __name__ == '__main__':
     app.run(debug=True)
